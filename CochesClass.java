5d90c943b91546269eeff8ca2490f5e54cd794df
Flor <l24120399@morelia.tecnm.mx>
2024-09-09 19:06:15 -0600

    Primer avance del código (aún tiene errores)

---
+++ CochesClass.java
@@ -0,0 +1,26 @@
+public class Coche {
+
+    String marca;
+    String modelo;
+    int año;
+
+    public Coche(String marca, String modelo, int año) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+    }
+        public void MostrarInfo() {
+
+            System.out.println("Marca: " + marca);
+            System.out.println("Modelo: " + modelo);
+            System.out.println("Año: " + año);
+
+        }
+
+        public int CalcularEdadDelCoche(int añoActual)
+        {
+            int añoA=2024;
+            int edad=añoA-año;
+            System.out.println("Este carro tiene: "+edad+" años.");
+        }return edad;
+}

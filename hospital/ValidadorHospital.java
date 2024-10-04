a16a35bf68728c602938483adf0017bddf29234b
Flor <l24120399@morelia.tecnm.mx>
2024-10-03 18:25:21 -0600

    Tarea 1 de la unidad 3 terminada

---
+++ hospital/ValidadorHospital.java
@@ -0,0 +1,64 @@
+package hospital;
+
+import consultas.Consulta;
+import usuarios.medicos.Medico;
+import usuarios.pacientes.Paciente;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class ValidadorHospital {
+
+    public boolean validarDisponibilidadEnFecha(
+            LocalDateTime fechaDeseada,
+            int numeroConsultorio,
+            ArrayList<Consulta> listaConsultas) {
+
+        for (Consulta consulta : listaConsultas) {
+            if (consulta.getFechaHora().isEqual(fechaDeseada) &&
+                    numeroConsultorio == consulta.getConsultorio().getNumeroConsultorio()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validarDisponibilidadMedico(
+            LocalDateTime fechaDeseada,
+            String idMedico,
+            ArrayList<Consulta> listConsultas
+    ) {
+        for (Consulta consulta : listConsultas) {
+            if (consulta.getFechaHora().isEqual(fechaDeseada)
+                    && consulta.getMedico().getId().equals(idMedico)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public boolean validarFechaCorrecta(LocalDateTime fechaDeseda) {
+
+        if (fechaDeseda.isBefore(LocalDateTime.now())) return false;
+        return true;
+    }
+
+    public boolean validarTelefono(String telefonoDeseado, ArrayList<Paciente> listaPacientes) {
+
+        for (Paciente paciente : listaPacientes) {
+
+            if (paciente.getTelefono().equals(telefonoDeseado)) return false;
+
+        }
+        return true;
+    }
+    public boolean validarRFC(String RFC, ArrayList<Medico> listaMedicos) {
+
+        for (Medico medico : listaMedicos) {
+
+            if (medico.getRfc().equals(RFC)) return false;
+
+        }
+        return true;
+    }
+}

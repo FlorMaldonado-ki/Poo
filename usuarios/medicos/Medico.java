58e9b5164652c12989ae509aa889b469a8c9d6d2
Flor <l24120399@morelia.tecnm.mx>
2024-10-22 21:13:49 -0600

    Tarea 4 terminada

---
+++ usuarios/medicos/Medico.java
@@ -0,0 +1,38 @@
+package usuarios.medicos;
+
+import usuarios.Usuario;
+import usuarios.utils.Rol;
+
+import java.time.LocalDate;
+import java.util.Random;
+
+public class Medico extends Usuario {
+
+
+        private String telefono;
+        private String rfc;
+        Random rand = new Random();
+
+        public Medico(String id, String nombre, String apellidos, LocalDate fechaNacimiento,
+                      String telefono, String rfc, String contraseña) {
+
+            super(id, nombre, apellidos, fechaNacimiento, telefono, contraseña, Rol.MEDICO);
+            this.rfc = rfc;
+
+        }
+        @Override
+        public String mostrarInformacion(){
+
+            return super.mostrarInformacion() + String.format(", RFC: %s, ", this.rfc);
+        }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+
+}
